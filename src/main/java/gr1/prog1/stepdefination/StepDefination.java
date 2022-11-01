6696d96c51daea30bcea663c885f31bd62f6256a
vidya sagar <pudattu_vidyasagar@infosys.com>
2022-11-02 00:50:45 +0530

    first commit

---
+++ src/main/java/gr1/prog1/stepdefination/StepDefination.java
@@ -0,0 +1,35 @@
+package gr1.prog1.stepdefination;
+
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class StepDefination {
+	public WebDriver driver = null;
+		
+	@When("^click  on sign in link")
+	public void click_on_sign_in_link() throws Throwable{
+		System.out.println("closing driver");
+		driver.close();
+	}
+	
+	@Given("^navigate to utest url$")
+	public void navigate_to_utest_url() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+		System.out.println("navigate");
+		
+		WebDriverManager.chromedriver().driverVersion("107").setup();
+		driver = new ChromeDriver();
+		// Navigate to the demoqa website
+		driver.get("https://www.demoqa.com");
+		Thread.sleep(5000);
+	}
+
+}
---
+++ target/classes/ui/features/Login.feature
@@ -0,0 +1,5 @@
+Feature: validate the login functionality
+
+ Scenario: validate the negative login functionality
+ Given navigate to utest url
+ When click  on sign in link
